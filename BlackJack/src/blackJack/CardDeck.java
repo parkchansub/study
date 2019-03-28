7ac15cd6d3ccb570b0649891df88de811dd8d3f7
cs.park <cs.park@quintet.co.kr>
2019-03-28 11:16:19 +0900

    테스트

--- BlackJack/src/blackJack/CardDeck.java
+++ BlackJack/src/blackJack/CardDeck.java
@@ -1,5 +1,11 @@
 package blackJack;
 
-public class CardDeck {
+import java.util.List;
 
+public class CardDeck {
+    private List<Card> cards;
+
+    public Card getCard(){
+        return null;
+    }
 }
