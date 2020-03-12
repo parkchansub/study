0dbe2d43b8560292748cbeef22684cd2ff8e3b1f
cs.park <pcs0331@naver.com>
2020-03-13 02:00:57 +0900

    4장 오류 수정 application 위치 문제 및 오타 문제(error creating bean with name)

--- src/main/java/com/jojoldu/book/springboot/Application.java
+++ src/main/java/com/jojoldu/book/springboot/Application.java
@@ -1,4 +1,4 @@
-package com.jojoldu.book.springboot.web;
+package com.jojoldu.book.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
