324ac301214bd726ee45e7dc668b9124fe04f049
cs.park <pcs0331@naver.com>
2020-03-16 00:40:01 +0900

    inxdex.js 오류 해결

--- src/main/java/com/jojoldu/book/springboot/web/HelloController.java
+++ src/main/java/com/jojoldu/book/springboot/web/HelloController.java
@@ -13,7 +13,7 @@ public class HelloController {
         return "hello";
     }
 
-    @GetMapping
+    @GetMapping("/hello/dto")
     public HelloResponseDto helloDto(@RequestParam("name") String name, @RequestParam("amount") int amount){
         return new HelloResponseDto(name, amount);
     }
