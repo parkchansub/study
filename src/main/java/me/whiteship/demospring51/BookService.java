a39e3628793d66b32a59ec67e78246c1b766dbaa
cs.park <cs.park@quintet.co.kr>
2020-07-20 17:57:09 +0900

    핵심 기술 3장 완료 (@Autowired)

--- src/main/java/me/whiteship/demospring51/BookService.java
+++ src/main/java/me/whiteship/demospring51/BookService.java
@@ -1,6 +1,7 @@
 package me.whiteship.demospring51;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 @Service
