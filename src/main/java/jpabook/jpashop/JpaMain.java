0e544963d381a328e60c7718f44179452d583bfe
cs.park <pcs0331@naver.com>
2020-08-18 04:32:09 +0900

    연관관계 매핑 기초 - 단반향 연관관계 까지

--- src/main/java/jpabook/jpashop/JpaMain.java
+++ src/main/java/jpabook/jpashop/JpaMain.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop;
 
 import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.Team;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -19,6 +20,20 @@ public class JpaMain {
         tx.begin();
 
         try {
+
+            Team team = new Team();
+            team.setName("TeamA");
+            em.persist(team);
+
+            Member member = new Member();
+            member.setName("member1");
+            member.setTeam(team);
+            em.persist(member);
+
+            Member findMember = em.find(Member.class, member.getId());
+
+            Team findTeam = findMember.getTeam();
+            findTeam.getName();
 
             tx.commit();
         } catch (Exception e) {
