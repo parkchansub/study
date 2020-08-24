a2a70bdabcad2a7c935e36d33d7631195be20f2a
cs.park <cs.park@quintet.co.kr>
2020-08-24 16:51:55 +0900

    commit test

--- src/main/java/jpabook/jpashop/JpaMain.java
+++ src/main/java/jpabook/jpashop/JpaMain.java
@@ -9,6 +9,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.time.LocalDateTime;
 import java.util.List;
 
 public class JpaMain {
@@ -23,6 +24,11 @@ public class JpaMain {
         tx.begin();
 
         try {
+
+            Member member = new Member();
+            member.setCreatedBy("park");
+            member.setCreatedDate(LocalDateTime.now());
+
 
             Order order = new Order();
             order.addOrderItem(new OrderItem());
