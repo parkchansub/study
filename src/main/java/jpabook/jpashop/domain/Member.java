50aef5911daed2e80c49f897f3f417bc7a0fa159
cs.park <cs.park@quintet.co.kr>
2020-08-28 17:32:41 +0900

    값 타입 코드 수정

--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -2,7 +2,9 @@ package jpabook.jpashop.domain;
 
 import javax.persistence.*;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Entity
 public class Member extends BaseEntity {
@@ -24,6 +26,25 @@ public class Member extends BaseEntity {
     private Period workPeriod;
 
     private Address homeAddress;
+
+    @ElementCollection
+    @CollectionTable(name="FAVORITE_FOOD" , joinColumns =
+    @JoinColumn(name = "MEMBER_ID")
+    )
+    @Column(name = "FOOD_NAME")
+    private Set<String> favoriteFoods = new HashSet<>();
+
+    /*@ElementCollection
+    @CollectionTable(name="ADDRESS", joinColumns =
+    @JoinColumn(name = "MEMBER_ID")
+    )
+    private List<Address> addresseHistory = new ArrayList<>();*/
+
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "MEMBER_ID")
+    private List<AddressEntity> ddresseHistory = new ArrayList<>();
+
 
 
     @OneToOne
