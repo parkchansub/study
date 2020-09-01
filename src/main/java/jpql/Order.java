ef8d108f5f30286c2e463ec581b994914692a136
cs.park <cs.park@quintet.co.kr>
2020-09-01 17:25:03 +0900

    Initial commit

---
+++ src/main/java/jpql/Order.java
@@ -0,0 +1,53 @@
+package jpql;
+
+import javax.persistence.*;
+
+@Entity
+public class Order {
+
+    @Id @GeneratedValue
+
+    private Long id;
+
+    private int orderamount;
+
+    @Embedded
+    private Address address;
+
+
+    @ManyToOne
+    @JoinColumn(name = "PRODUCT_ID")
+    private Product product;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getOrderamount() {
+        return orderamount;
+    }
+
+    public void setOrderamount(int orderamount) {
+        this.orderamount = orderamount;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+}
