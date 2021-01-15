acb9b8c23e61228cab9d8c1f7ea4fa03b157bf5e
cs.park <pcs0331@naver.com>
2021-01-15 23:52:23 +0900

    프로토 타입 스코프 까지 완료

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,13 +1,18 @@
 package hello.core.lifecycle;
 
-public class NetworkClient {
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+public class NetworkClient  {
 
     private String url;
 
     public NetworkClient() {
         System.out.println("생성자 호출 url = " + url);
-        connect();
-        call("초기화 연결 메세지");
+
     }
 
     public void setUrl(String url) {
@@ -25,5 +30,16 @@ public class NetworkClient {
     public void disconnect() {
         System.out.println("close: " + url);
 
+    }
+
+    @PostConstruct
+    public void init() throws Exception {
+        connect();
+        call("초기화 연결 메세지");
+    }
+
+    @PreDestroy
+    public void close() throws Exception {
+        disconnect();
     }
 }
