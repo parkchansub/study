99055bcbc99152ab42ae6775ac4ae46a5ce35531
cs.park <pcs0331@naver.com>
2021-09-06 18:17:52 +0900

    타임리프 - 스프링 통합과 폼 완료

--- src/main/java/hello/itemservice/domain/item/ItemType.java
+++ src/main/java/hello/itemservice/domain/item/ItemType.java
@@ -6,6 +6,10 @@ public enum ItemType {
 
     private final String description;
 
+    public String getDescription() {
+        return description;
+    }
+
     ItemType(String description) {
         this.description = description;
     }
