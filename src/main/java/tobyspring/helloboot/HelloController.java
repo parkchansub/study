dec8817a98fb06a271635ff0b886418f5067dfb0
DESKTOP-APIJFLV\pcs <pcs0331@naver.com>
2023-02-01 09:23:34 +0900

    독립 실행형 서블릿 애플리케이션 완료

---
+++ src/main/java/tobyspring/helloboot/HelloController.java
@@ -0,0 +1,11 @@
+package tobyspring.helloboot;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+public class HelloController {
+
+    public String hello(String name) {
+        return "hello " + name;
+    }
+}
--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -1,13 +1,43 @@
 package tobyspring.helloboot;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 
-@SpringBootApplication
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 public class HellobootApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HellobootApplication.class, args);
+
+		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+
+		WebServer webServer  = serverFactory.getWebServer(servletContext -> {
+			servletContext.addServlet("frontController", new HttpServlet() {
+				@Override
+				protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+					if (req.getRequestURI().equals("hello") && req.getMethod().equals(HttpMethod.GET)) {
+						String name = req.getParameter("name");
+						resp.setStatus(200);
+						resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+						resp.getWriter().println("Hello " + name);
+
+					} else if (req.getRequestURI().equals("/user")) {
+
+					} else {
+						resp.setStatus(HttpStatus.NOT_FOUND.value());
+					}
+				}
+			}).addMapping("/*");
+		});
 	}
 
 }
