c41192e6e1789b902deae24128af36c1934be42f
pcs0331 <pcs0331@naver.com>
2023-04-11 22:25:14 +0900

    자바 예외 이해 완료

--- src/test/java/hello/jdbc/exception/basic/UnCheckedAppTest.java
+++ src/test/java/hello/jdbc/exception/basic/UnCheckedAppTest.java
@@ -4,67 +4,53 @@ import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.net.ConnectException;
 import java.sql.SQLException;
 
 @Slf4j
 public class UnCheckedAppTest {
 
     @Test
-    void unchecked_catch() {
-        Service service = new Service();
-        service.callCatch();
-
+    void unchecked() {
+        Controller controller = new Controller();
+        Assertions.assertThatThrownBy(() -> controller.request())
+                .isInstanceOf(RuntimeConnectionException.class);
     }
 
     @Test
-    void unchecked_throw() {
+    void printEx() {
+        Controller controller = new Controller();
+        try {
+            controller.request();
+        } catch (Exception e) {
+            log.info("ex", e);
+        }
+    }
+
+
+    static class Controller {
         Service service = new Service();
-        service.callCatch();
-        Assertions.assertThatThrownBy(() -> service.callThrow())
-                .isInstanceOf(MyUnCheckedException.class);
-    }
+        public void request() {
+            service.logic();
 
-    /**
-     * RuntimeException을 상속받은 예외는 언체크 예외가 된다.
-     */
-    static class MyUnCheckedException extends RuntimeException {
-        public MyUnCheckedException(String message) {
-            super(message);
         }
     }
 
-    /**
-     * UnChecked 에외는
-     * 예외를 잡거나, 던지지 않아도 된다.
-     * 예외를 잡지 않으면 자도응로 밖으로 던진다.
-     */
-    static class Service{
+    static class Service {
+
         Repository repository = new Repository();
+        NetworkClient networkClient = new NetworkClient();
 
-        public void callCatch() {
-            try {
-                repository.call();
-            } catch (MyUnCheckedException e) {
-                //예외 로직 처리
-                log.info("예외처리 , message = {}", e.getMessage(),e);
-            }
-        }
-
-        /**
-         * 예외를 잡지 않아도 된다. 자연스럽게 상위로 넘어간다.
-         * 체크 예외와 다르게 throws 예외 선언을 하지 않아도 된다.
-         */
-        public void callThrow() {
+        public void logic() {
             repository.call();
+            networkClient.call();
 
         }
 
     }
 
     static class NetworkClient{
-        public void call() throws ConnectException {
-            throw new RuntimeConnectionException("ex");
+        public void call(){
+            throw new RuntimeConnectionException("연결 실패");
         }
     }
 
