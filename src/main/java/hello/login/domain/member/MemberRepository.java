d2a45e47761bbf78382541ab70175065f086daad
pcs0331 <pcs0331@naver.com>
2023-04-24 22:49:20 +0900

    Initial commit

---
+++ src/main/java/hello/login/domain/member/MemberRepository.java
@@ -0,0 +1,33 @@
+package hello.login.domain.member;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Slf4j
+@Repository
+public class MemberRepository {
+
+    private static Map<Long, Member> store = new HashMap<>();
+    private static Long sequence = 0L;
+    public Member save(Member member) {
+        member.setId(++sequence);
+        log.info("save member = {} ", member);
+        store.put(member.getId(), member);
+        return member;
+
+    }
+
+    public Member findById(Long id) {
+        return store.get(id);
+    }
+
+    public List<Member> findAll() {
+        return new ArrayList<>();
+    }
+
+}
