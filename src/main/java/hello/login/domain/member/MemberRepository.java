b5f6dfc72e5fd280d0d6cf5c3d2172489edff63c
pcs0331 <pcs0331@naver.com>
2023-05-29 23:37:58 +0900

    서블릿 필터 - 인증 체크

--- src/main/java/hello/login/domain/member/MemberRepository.java
+++ src/main/java/hello/login/domain/member/MemberRepository.java
@@ -3,10 +3,7 @@ package hello.login.domain.member;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @Slf4j
 @Repository
@@ -14,6 +11,7 @@ public class MemberRepository {
 
     private static Map<Long, Member> store = new HashMap<>();
     private static Long sequence = 0L;
+
     public Member save(Member member) {
         member.setId(++sequence);
         log.info("save member = {} ", member);
@@ -26,8 +24,20 @@ public class MemberRepository {
         return store.get(id);
     }
 
+    public Optional<Member> findByLoginId(String loginId) {
+        return findAll().stream()
+                .filter(m -> m.getLoginId().equals(loginId))
+                .findFirst();
+    }
+
+
     public List<Member> findAll() {
         return new ArrayList<>();
     }
 
+
+    public void clearStore() {
+        store.clear();
+
+    }
 }
