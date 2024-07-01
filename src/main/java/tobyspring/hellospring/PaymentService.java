6c33583da2573241824632ba6b475d66379d7557
박찬섭 <pcs0331@naver.com>
2024-07-01 09:05:24 +0900

    1. 섹션 2. 원칙과 패턴

--- src/main/java/tobyspring/hellospring/PaymentService.java
+++ src/main/java/tobyspring/hellospring/PaymentService.java
@@ -1,9 +1,13 @@
 package tobyspring.hellospring;
 
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
 import java.io.IOException;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
+@Component
 public class PaymentService {
     private final ExRateProvider exRateProvider;
 
