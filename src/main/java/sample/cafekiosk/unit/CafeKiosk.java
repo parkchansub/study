de6ebd63daffe2297410e174d7f326028c904656
pcs <pcs0331@naver.com>
2024-07-18 13:01:26 +0900

    Initial commit

---
+++ src/main/java/sample/cafekiosk/unit/CafeKiosk.java
@@ -0,0 +1,50 @@
+package sample.cafekiosk.unit;
+
+import org.aspectj.weaver.ast.Or;
+import sample.cafekiosk.unit.beverage.Beverage;
+import sample.cafekiosk.unit.order.Order;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CafeKiosk {
+
+    private final List<Beverage> beverages = new ArrayList<>();
+
+    public void add(Beverage beverage) {
+        beverages.add(beverage);
+    }
+
+
+    public void add(Beverage beverage, int count) {
+        if (count <= 0) {
+            throw new IllegalArgumentException("음료는 1잔 이상 주문하실 수 있습니다.");
+        }
+
+        for (int i = 0; i < count; i++) {
+            beverages.add(beverage);
+        }
+    }
+
+
+    public void remove(Beverage beverage) {
+        beverages.remove(beverage);
+    }
+
+    public void clear() {
+        beverages.clear();
+    }
+
+    public int calculateTotalPrice() {
+        return beverages.stream().mapToInt(Beverage::getPrice).sum();
+    }
+
+    public Order createOrder() {
+        return new Order(LocalDateTime.now(), beverages);
+    }
+
+    public List<Beverage> getBeverages() {
+        return beverages;
+    }
+}
